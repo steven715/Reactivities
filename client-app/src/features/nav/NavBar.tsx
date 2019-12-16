a6d9426abfca26ac4a8ab67a5d74037da4e92c63
react-tutorial <yugioh821113@gmail.com>
2019-12-16 23:59:50 +0800

    End of section 5

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Menu, Container, Button } from 'semantic-ui-react'
+
+interface IProps {
+    openCreateForm: () => void;
+}
+
+export const NavBar: React.FC<IProps> = ({openCreateForm}) => {
+    return (
+        <Menu fixed='top' inverted>
+            <Container>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}} />
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item name='messages' />
+                <Menu.Item>
+                    <Button onClick={openCreateForm} positive content='Create Activities' />
+                </Menu.Item>
+            </Container>
+
+        </Menu>
+    );
+};
