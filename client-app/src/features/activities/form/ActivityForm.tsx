ff4c4bd71d07fec5fedf9536d1e7c239bdd4ba3d
react-tutorial <yugioh821113@gmail.com>
2019-12-17 22:25:02 +0800

    End of section 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,20 +1,22 @@
 import React, { useState, FormEvent } from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 
 interface IProps {
     setEditMode: (editMode: boolean) => void;
     activity: IActivity;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean;
 }
 
 export const ActivityForm: React.FC<IProps> = ({
     setEditMode,
     activity: initialFormState,
     createActivity,
-    editActivity
+    editActivity,
+    submitting
 }) => {
     const initializeForm = () => {
         if (initialFormState) {
@@ -93,6 +95,7 @@ export const ActivityForm: React.FC<IProps> = ({
                     value={activity.venue}
                 />
                 <Button
+                    loading={submitting}
                     floated='right'
                     positive
                     type='submit'
